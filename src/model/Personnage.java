5085e216c18377594d6ba340a31af16546032e97
laurent6 <lau.lau88@hotmail.fr>
2017-11-21 17:39:51 +0100

    change

--- src/model/Personnage.java
+++ src/model/Personnage.java
@@ -9,30 +9,49 @@ public abstract class Personnage extends Object {
 	public Personnage(Board board){
 		super(board);
 	}
+	/**
+	 * 
+	 * @return test si le personnage peu aller sur la gauche
+	 */
 	private boolean canLeft(){
 		if(this.getPosX() > 0)
 		return !board.getDoor().containsEdge(ref[this.getPosX()][this.getPosY()], ref[this.getPosX()-1][this.getPosY()]);
 		
 		return false;
 	}
+	/**
+	 * 
+	 * @return test si le personnage peu se déplacer a droite. 
+	 */
 	private boolean canRight(){
 		if(this.getPosX() < board.getSize()-1)
 		return !board.getDoor().containsEdge(ref[this.getPosX()][this.getPosY()], ref[this.getPosX()+1][this.getPosY()]);
 		
 		return false;
 	}
+	/**
+	 * 
+	 * @return test si le personnage peut aller en haut
+	 */
 	private boolean canUp(){
 		if(this.getPosY() > 0)
 		return !board.getDoor().containsEdge(ref[this.getPosX()][this.getPosY()], ref[this.getPosX()][this.getPosY()-1]);
 		
 		return false;
 	}
+	/**
+	 * 
+	 * @return test si le personnage peu aller en bas.
+	 */
 	private boolean canDown(){
 		if(this.getPosY() < board.getSize()-1)
 		return !board.getDoor().containsEdge(ref[this.getPosX()][this.getPosY()], ref[this.getPosX()][this.getPosY()+1]);
 		
 		return false;
 	}
+	/**
+	 * deplacement a droite du personnage 
+	 */
 	public void moveRight(){
 		if(this.canRight()){
 			this.setPosition(new Point2D(position.getX()+1, position.getY()));
