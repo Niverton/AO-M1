08e2f6ad5ac7c61b04809624e2f11cc9847730e3
Badr-Eddine Faik <bfaik@emi.u-bordeaux1.fr>
2017-12-05 17:03:00 +0100

    door

---
+++ src/controller/DoorController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.scene.layout.Pane;
+
+import view.DoorView;
+public class DoorController  {
+		
+		private DoorView d;
+		
+		public DoorController( ){
+		
+		
+		}
+		/**
+		 * 
+		 * @param pane Le pane du jeux.
+		 */
+		public void start(Pane pane){
+		    d = new DoorView(pane);
+			d.view();
+		}
+
+		public void update() {
+			// TODO Auto-generated method stub
+			
+			d.uptdate();
+		}
+
+}
---
+++ src/model/Door.java
@@ -0,0 +1,19 @@
+package model;
+
+
+
+public class Door extends NonMoveableObject {
+
+	public Door(String porte) {
+		super(porte);
+		
+	}
+	public Door(String porte,int x, int y) {
+		super(porte, x,y);
+	}
+	/*public static Door getInstance(java.lang.Object board) {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+
+}
