c5ae7dae02333295f6db215d55f597ff59a00275
Niverton <niverton@live.fr>
2017-12-22 18:18:03 +0100

    GraphTest légère avancée

--- src/test/GraphTest.java
+++ src/test/GraphTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
@@ -11,7 +13,7 @@ import model.graph.*;
 
 public class GraphTest {
 	Graph g;
-	Vertex v1,v2,v3,v4,v5;
+	Vertex v1,v2,v3,v4,v5,v6;
 	Edge e1,e2,e3,e4;
 	
     @Before
@@ -22,10 +24,11 @@ public class GraphTest {
     	g.addVertex(v3 = new Vertex(2,1,2));
     	g.addVertex(v4 = new Vertex(1,0,3));
     	g.addVertex(v5 = new Vertex(1,2,4));
+    	g.addVertex(v6 = new Vertex(0,2,5));
     	e1 = g.addEdge(v1, v2);
     	e2 = g.addEdge(v1, v3);
     	e3 = g.addEdge(v1, v4);
-    	e4 = g.addEdge(v1, v5);
+    	g.addEdge(v1, v5, new Edge(Edge.Type.OPENED_DOOR));
     }
 
     @After
@@ -45,5 +48,20 @@ public class GraphTest {
     	public Vertex randomVertex();
 
     	public boolean isOpenDoor(Vertex vertex, Directions dir);*/
+        
+        //Exists et gettarget
+        assertTrue(g.exists(v1, Directions.West)); //v2
+        assertFalse(g.exists(v5, Directions.North)); //Rien
+        
+        //isConnected et getrefvertex
+        assertTrue(g.isConnected(v1, Directions.West)); //v2
+        assertFalse(g.isConnected(v2, Directions.North)); //v6 mais pas relié
+        
+        //randomVertex
+        assertNotNull(g.randomVertex());
+        
+        //isOpenDoor
+        assertFalse(g.isOpenDoor(v1, Directions.South));
+        assertTrue(g.isOpenDoor(v1, Directions.North)); //v5 porte
     }
 }
